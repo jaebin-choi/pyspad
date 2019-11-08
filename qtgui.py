93f93f86e94ad47095351e13a444075c15c55d24
Jaebin Choi <jc3797@columbia.edu>
2019-11-08 18:58:21 -0500

    qtgui.py activates parse.py upon run button and plots in main GUI. Next: 1) fix parse.py to have directories as input --- DONE 20191108 2) opal kelly flash and data acquisition ---- START HERE NEXT 3) parsephasesweep.py, and connect 4) live mode

--- qtgui.py
+++ qtgui.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import QDateTime, Qt, QTimer
+from PyQt5.QtCore import QDateTime, Qt, QTimer, pyqtSlot
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
         QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
@@ -7,7 +7,7 @@ from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
         QVBoxLayout, QWidget)
 import pyqtgraph as pg
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 
 # pg.setConfigOption('background', 'w')
@@ -41,31 +41,35 @@ class WidgetGallery(QDialog):
         #set axes
         self.setPlotWidget(self.pw1, 0, 512, 0, 512, 'Pixel', 'Counts', '', '')
 
+        #if button is clicked
         self.runButton.clicked.connect(self.ifButtonClicked)
 
+    @pyqtSlot()
     def ifButtonClicked(self):
         import parse
         # inputBitfile = QLineEdit.text(self.lineEdit1)
-        # inputSavedir = QLineEdit.text(self.lineEdit2)
-        # inputNframes = QLineEdit.text(self.lineEdit3)
+        inputBitfile = self.lineEdit1.text()
+        inputSavedir = self.lineEdit2.text()
+        inputNframes = self.lineEdit3.text()
 
         #declare variables (will be integrated)
         npix=512
         datasize=1000
         ignoreframes=10
         # dir='D:\\dropbox\\Dropbox\\Projects\\SpadProbe\\git\\pyspad\\rawdata\\'
-        dir='D:\\dropbox\\Dropbox\\Projects\\SpadProbe\\201907_caltech5\\codepackage_aftertrip\\rawdata\\'
-        iframe=0
+        # dir='D:\\dropbox\\Dropbox\\Projects\\SpadProbe\\201907_caltech5\\codepackage_aftertrip\\rawdata\\'
+        dir='D:\\dropbox\\Dropbox\\Projects\\SpadProbe\\201902_caltechGcamp\\code package\\outfile\\0223\\0223_recording_LP57\\'
+        iframe=4
 
         #fetch data from parse module
-        [self.img, self.scatt, self.goodframes] = parse.Parse(npix, datasize, ignoreframes, dir, iframe).get_data()
+        [self.img, self.scatt, self.goodframes] = parse.Parse(npix, datasize, ignoreframes, inputSavedir, int(inputNframes)).get_data()
 
         #update plots with parsed data
         self.pw2.plot(self.img) #flattened image
         self.pw3.plot(np.tile(range(0,npix),self.goodframes), self.scatt[0:self.goodframes*npix]) #raw count scatterplot
 
-        self.setPlotWidget(self.pw2, 0, 512, 0, self.goodframes*63, 'Pixel', 'Accumulated Counts', '', '')
-        self.setPlotWidget(self.pw3, 0, 512, 0, 64, 'Pixel', 'Flattened Counts', '', '')
+        self.setPlotWidget(self.pw2, 0, 512, 0, max(self.img), 'Pixel', 'Accumulated Counts', '', '')
+        self.setPlotWidget(self.pw3, 0, 512, 0, max(self.scatt), 'Pixel', 'Flattened Counts', '', '')
 
     def setPlotWidget(self, plot, xmin, xmax, ymin, ymax, xlabel, ylabel, xunit, yunit):
         plot.setLabel('bottom', xlabel, units=xunit)
@@ -89,17 +93,18 @@ class WidgetGallery(QDialog):
         layoutOption.addWidget(radioButton1)
         layoutOption.addWidget(radioButton2)
 
-        lineEdit1 = QLineEdit('enter full path of bitfile')
-        lineEdit2 = QLineEdit('enter full path of save directory')
-        lineEdit3 = QLineEdit('enter number of frames to acquire 1-100000')
+        self.lineEdit1 = QLineEdit(self)
+        # lineEdit1 = QLineEdit('enter full path of bitfile')
+        self.lineEdit2 = QLineEdit('D:\\dropbox\\Dropbox\\Projects\\SpadProbe\\201902_caltechGcamp\\code package\\outfile\\0223\\0223_recording_LP57\\')
+        self.lineEdit3 = QLineEdit('4')
 
         titleOption = QLabel("Acq Option")
         title1 = QLabel('bitfile')
-        title1.setBuddy(lineEdit1)
+        title1.setBuddy(self.lineEdit1)
         title2 = QLabel('savedir')
-        title2.setBuddy(lineEdit2)
+        title2.setBuddy(self.lineEdit2)
         title3 = QLabel('#frames')
-        title3.setBuddy(lineEdit3)
+        title3.setBuddy(self.lineEdit3)
 
         self.runButton = QPushButton("Run")
         self.runButton.setDefault(False)
@@ -110,11 +115,11 @@ class WidgetGallery(QDialog):
         layout.addWidget(titleOption, 0,0)
         layout.addLayout(layoutOption, 0,1)
         layout.addWidget(title1, 1,0)
-        layout.addWidget(lineEdit1, 1,1, 1,20) #last two ints define relative stretch conditions
+        layout.addWidget(self.lineEdit1, 1,1, 1,20) #last two ints define relative stretch conditions
         layout.addWidget(title2, 2,0)
-        layout.addWidget(lineEdit2, 2,1, 1,20)
+        layout.addWidget(self.lineEdit2, 2,1, 1,20)
         layout.addWidget(title3, 3,0)
-        layout.addWidget(lineEdit3, 3,1, 1,20)
+        layout.addWidget(self.lineEdit3, 3,1, 1,20)
         layout.addWidget(self.runButton,4,0)
 
         self.controlGroupBox.setLayout(layout)
